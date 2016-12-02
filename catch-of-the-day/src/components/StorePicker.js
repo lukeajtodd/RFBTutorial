083d9a43a58f33f8dfdc2626c74f851edb2e3098
Luke Todd <luke.todd@plus.net>
2016-12-02 17:25:55 +0000

    video 11 broken

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import { getFunName } from '../helpers';
 
 export default class StorePicker extends Component {
+    goToStore(e) {
+        e.preventDefault();
+        console.log("You have done it");
+    }
     render() {
         return (
-            <form className="store-selector">
+            <form onSubmit={this.goToStore(e)} className="store-selector">
                 <h2>Please enter a store</h2>
-                <input type="text" required placeholder="Store Name"/>
+                <input type="text" required placeholder="Store Name" defaultValue={ getFunName() } ref={(input) => {this.storeInput = input}}/>
                 <button type="submit">Visit Store</button>
             </form>
         )
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import './css/style.css';
-import App from './components/App';
+import { BrowserRouter, Match, Miss } from 'react-router';
 
-render(<App />, document.getElementById('main'));
+import './css/style.css';
+
+import App from './components/App';
+import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
+
+const Root = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <Match exactly pattern='/' component={StorePicker}/>
+                <Match pattern='/store/:storeId' component={App} />
+                <Miss component={NotFound} />
+            </div>
+        </BrowserRouter>
+    )
+}
+
+render(<Root />, document.getElementById('main'));
